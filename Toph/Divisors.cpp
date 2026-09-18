6ef4f7642183d19ed3558c270e3fe81a942a0b23
Debarpon Barman <151996346+debarponbarmanturjo@users.noreply.github.com>
2026-09-18 20:10:36 +0600

    Add Divisors program in Toph/Divisors.cpp
    Implement a program to print all divisors of a number.

---
+++ Toph/Divisors.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++){
+        if (n % i == 0) {
+            printf("%d\n", i);
+        }
+    }
+
+    return 0;
+}
