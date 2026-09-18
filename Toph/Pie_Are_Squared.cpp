167217988a1cf37ff70144cf62f4b0b39679d3ee
Debarpon Barman <151996346+debarponbarmanturjo@users.noreply.github.com>
2026-09-18 19:20:45 +0600

    Add Pie_Are_Squared.cpp to calculate area of a circle

---
+++ Toph/Pie_Are_Squared.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+    double r;
+
+    scanf("%lf", &r);
+    printf("%.10f\n", 3.141592653589793*r*r );
+
+    return 0;
+}
